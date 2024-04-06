b1aa93b968430e0130f0f9c2cd54f20cccd75612
Rashid <rashidpk090@gmail.com>
2024-04-06 13:57:58 +0530

    done

--- src/pages/services/sidebar.jsx
+++ src/pages/services/sidebar.jsx
@@ -8,7 +8,7 @@ const sidebar = () => {
     
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-1 md:gap-5 gap-8'>
-        <div className='flex flex-col gap-y-[0.10rem] h-fit bg-primary  font-semibold text-lg uppercase'>
+        <div className='flex flex-col gap-y-[0.10rem] h-fit bg-primary  font-semibold text-sm xl:text-base uppercase'>
                {ProductsData?.map((url,i)=>(
                  <Link to={`/services/${url?.url}`} key={i} className={`${location.pathname === `/services/${url.url}` ? 'bg-primary text-white' : 'bg-gray-200 text-primary'} w-full px-7 py-3  border-primary`}>{url?.title}</Link>
                ))}
@@ -18,7 +18,7 @@ const sidebar = () => {
                     <Link className={`w-full px-7 py-3  border-primary bg-gray-200 text-primary`}>BRAKE LININGS</Link> */}
 
         </div>
-        <div className='  bg-primary text-white px-7 md:px-4 md:py-4 xl:p-10 py-10 flex flex-col gap-5'>
+        <div className='h-fit  bg-primary text-white px-7 md:px-4 md:py-4 xl:p-10 py-10 flex flex-col gap-5'>
             <h3 className='text-2xl xl:text-3xl font-semibold'>WE ARE HERE TO HELP</h3>
             <p className='text-sm '>Lorem ipsum dolor sit amet, consectetur adipiscing
             elit, sed do eiusmod tempor incididunt ut labore et
